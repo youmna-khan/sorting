d36bcd11e67b71f98b39f6cd5d2b433de422718c
Youmna Naseem <youmnakhan23@gmail.com>
2022-05-19 23:48:47 +0100

    added test case for sorting

--- src/test/java/com/start/SorterFactoryTest.java
+++ src/test/java/com/start/SorterFactoryTest.java
@@ -10,8 +10,8 @@ class SorterFactoryTest {
 
   @Test
   void sorterFactoryTest() throws SorterLoaderException {
-    Assertions.assertEquals( "BubbleSorter",SorterFactory.getSorter( 1),getClass().getSimpleName() );
-    Assertions.assertEquals( "MergeSorter",SorterFactory.getSorter( 2),getClass().getSimpleName() );
+    Assertions.assertEquals( "BubbleSort", SorterFactory.getSorter( 1).getClass().getSimpleName() );
+    Assertions.assertEquals( "MergeSort",SorterFactory.getSorter( 2).getClass().getSimpleName() );
     //assertEquals( "BinarySorter",SorterFactory.getSorter( 3),getClass().getSimpleName() );
 
 
